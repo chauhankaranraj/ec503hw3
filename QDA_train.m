5f6aeee3153579ac53b2d6e04c974511c3da4da2
Karanraj Chauhan <chauhank@bu.edu>
2017-09-30 03:21:17 -0400

    updated faster, shorter version of qda

--- QDA_train.m
+++ QDA_train.m
@@ -29,38 +29,50 @@ QDAmodel.Mu = zeros(numofClass, D);
 QDAmodel.Sigma = zeros(D, D, numofClass);
 QDAmodel.Pi = zeros(numofClass, 1);
 
-% transpose once, use multiple times
-X_train_transpose = X_train';
-
 
 %%%%%%%%%%%%%%%%% CALCULATE CLASS MEAN, PRIOR PROB VECTORS %%%%%%%%%%%%%%%%
 
 for this_class = 1:numofClass
     
-    % boolean vector, 1 if data pt belongs to this class and 0 otherwise
-    is_data_pt_this_class = (Y_train == this_class);
-    num_this_class_data_pts = sum(is_data_pt_this_class);
+%     % boolean vector, 1 if data pt belongs to this class and 0 otherwise
+%     is_data_pt_this_class = (Y_train == this_class);
+%     num_this_class_data_pts = sum(is_data_pt_this_class);
+%     
+%     % update prior prob vector with prio prob estimate of this class
+%     QDAmodel.Pi(this_class) = num_this_class_data_pts/num_data_pts;
+%     
+%     if num_this_class_data_pts
+%         % get sum of each dimension, ONLY for data pts from this class
+%         % then divide by total num of data pts from this class
+%         % then transpose it to get class mean vector as a row
+%         QDAmodel.Mu(this_class,:) = ((X_train_transpose * is_data_pt_this_class)/num_this_class_data_pts)';
+%     
+%     
+%         % todo: matrix multiply instead of for loop?
+%     
+%     end
+
+    % get ONLY the data points that are labelled this_class
+    this_class_data_pts = X_train((Y_train == this_class), :);
     
-    % update prior prob vector with prio prob estimate of this class
-    QDAmodel.Pi(this_class) = num_this_class_data_pts/num_data_pts;
+    % get the row-wise mean (mean of each dimension) of all data points of this_class
+    QDAmodel.Mu(this_class,:) = mean(this_class_data_pts, 1);
     
-    if num_this_class_data_pts
-        % get sum of each dimension, ONLY for data pts from this class
-        % then divide by total num of data pts from this class
-        % then transpose it to get class mean vector as a row
-        QDAmodel.Mu(this_class,:) = ((X_train_transpose * is_data_pt_this_class)/num_this_class_data_pts)';
+    % set prior prob vector in qda model with prio prob estimate of this class
+    QDAmodel.Pi(this_class) = size(this_class_data_pts, 1) / num_data_pts;
     
-    
-        % todo: matrix multiply instead of for loop?
-    
+    % calculate class covariance matrix
+    class_cov_mat = zeros(D, D);
+
+    for data_pt_idx = 1:size(this_class_data_pts, 1)
+        temp = (this_class_data_pts(data_pt_idx,:)' - QDAmodel.Mu(this_class,:)');
+        class_cov_mat = class_cov_mat + (temp*temp');
     end
     
-
-end
-
-
-%%%%%%%%%%%%%%%% CALCULATE COVARIANCE MATRICES FOR CLASSES %%%%%%%%%%%%%%%%
-
-for this_class = 1:numofClasses
+    class_cov_mat = class_cov_mat/size(this_class_data_pts, 1);
     
+    % set the value of the calculated covariance matrix in qda model
+    QDAmodel.Sigma(:, :, this_class) = class_cov_mat;
+    
+end
 end
--- scratchpad.m
+++ scratchpad.m
@@ -1,36 +1,48 @@
-num_features = 3;
-numOfClass = 3;
+% num_features = 3;
+% numOfClass = 3;
+% 
+% % labels
+% Y_train = [1 2 3 2 2 2 2 1 3 2];
+% 
+% % 10 rows, 3 cols
+% X_train = rand(10,3);
+% disp(X_train);
+% 
+% % num_features x numOfClass matrix, i-th column is mean vector of i-th class
+% class_means = zeros(num_features, numOfClass);
+% 
+% for this_class = 1:numOfClass
+%     
+%     % num_data_points x 1 boolean vector, with 1 where class label is same as this_class
+%     is_pt_this_class = (Y_train == this_class)';
+%     num_this_class_pts = sum(is_pt_this_class);
+%     
+% %     disp(this_class);
+% %     disp(is_pt_this_class);
+%     
+%     if num_this_class_pts    
+%         % num_features x 1 vector, where i-th row is sum of feature #i of all data points
+%         this_class_feat_sum = (X_train') * is_pt_this_class;
+%         
+%         % divide feature sum by number of data_points
+%         class_means(:,this_class) = this_class_feat_sum/num_this_class_pts;
+%     else
+%         % no data points for this class, sum will be 0
+%         class_means(:,this_class) = zeros(num_features,1);
+%     end
+%     
+% end
+% 
+% disp(class_means);
 
-% labels
-Y_train = [1 2 3 2 2 2 2 1 3 2];
 
-% 10 rows, 3 cols
-X_train = rand(10,3);
-disp(X_train);
+train = rand(5,2);
+labels = [1 2 1 1 2]';
+disp(train);
 
-% num_features x numOfClass matrix, i-th column is mean vector of i-th class
-class_means = zeros(num_features, numOfClass);
+test = (train(labels(:)==1,:));
+disp(test);
 
-for this_class = 1:numOfClass
-    
-    % num_data_points x 1 boolean vector, with 1 where class label is same as this_class
-    is_pt_this_class = (Y_train == this_class)';
-    num_this_class_pts = sum(is_pt_this_class);
-    
-%     disp(this_class);
-%     disp(is_pt_this_class);
-    
-    if num_this_class_pts    
-        % num_features x 1 vector, where i-th row is sum of feature #i of all data points
-        this_class_feat_sum = (X_train') * is_pt_this_class;
-        
-        % divide feature sum by number of data_points
-        class_means(:,this_class) = this_class_feat_sum/num_this_class_pts;
-    else
-        % no data points for this class, sum will be 0
-        class_means(:,this_class) = zeros(num_features,1);
-    end
-    
-end
+mean_vec = mean(test, 1);
+disp(mean_vec);
 
-disp(class_means);
